9d6d620bfb0d9fea0e89c539417c7f1caea6ccca
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-03 22:40:58 +0300

    modified build

--- build/js/about.js
+++ build/js/about.js
@@ -1,7 +1,7 @@
 webpackJsonp([3],[/*!*******************!*\
   !*** multi about ***!
   \*******************/
-function(n,o,c){c(/*! ./utils.js */1),n.exports=c(/*! ./about_page.js */2)},,/*!***********************!*\
+function(n,o,c){c(/*! ./utils.js */1),n.exports=c(/*! ./about_page.js */4)},,,,/*!***********************!*\
   !*** ./about_page.js ***!
   \***********************/
 function(n,o){}]);
--- build/js/contact.js
+++ build/js/contact.js
@@ -1,8 +1,8 @@
-webpackJsonp([2],[/*!*********************!*\
+webpackJsonp([2],{0:/*!*********************!*\
   !*** multi contact ***!
   \*********************/
-function(n,o,c){c(/*! ./utils.js */1),n.exports=c(/*! ./contact_page.js */3)},,,/*!*************************!*\
+function(n,o,c){c(/*! ./utils.js */1),n.exports=c(/*! ./contact_page.js */5)},5:/*!*************************!*\
   !*** ./contact_page.js ***!
   \*************************/
-function(n,o){}]);
+function(n,o){}});
 //# sourceMappingURL=contact.js.map
