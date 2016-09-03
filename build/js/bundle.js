1cbf2f8b97ab2f34148ff7b6e140f104c2535e6b
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-03 20:33:20 +0300

    added source-map

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,1 +1,14 @@
-!function(t){function n(i){if(o[i])return o[i].exports;var r=o[i]={exports:{},id:i,loaded:!1};return t[i].call(r.exports,r,r.exports,n),r.loaded=!0,r.exports}var o={};return n.m=t,n.c=o,n.p="/public/assets/js/",n(0)}([function(t,n,o){o(2),t.exports=o(1)},function(t,n,o){!function(){"use strict";o(3),document.write("Welcome to Big Hair Concerts!")}()},function(t,n){!function(){"use strict"}()},function(t,n){"use strict";!function(){var t=function(t,n){};t("admin","idunno")}()}]);
+!function(t){function e(o){if(n[o])return n[o].exports;var r=n[o]={exports:{},id:o,loaded:!1};return t[o].call(r.exports,r,r.exports,e),r.loaded=!0,r.exports}var n={};return e.m=t,e.c=n,e.p="/public/assets/js/",e(0)}([/*!******************!*\
+  !*** multi main ***!
+  \******************/
+function(t,e,n){n(/*! ./utils */1),t.exports=n(/*! ./app */2)},/*!******************!*\
+  !*** ./utils.js ***!
+  \******************/
+function(t,e){"use strict"},/*!*****************!*\
+  !*** ./app.es6 ***!
+  \*****************/
+function(t,e,n){"use strict";var o=n(/*! ./login */3);(0,o.login)("admin","idunno"),document.write("Welcome to Big Hair Concerts!")},/*!*******************!*\
+  !*** ./login.es6 ***!
+  \*******************/
+function(t,e){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n=function(t,e){};e.login=n}]);
+//# sourceMappingURL=bundle.js.map
