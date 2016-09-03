79bc77c399368a32ee8a0bce737b27d3dab2b8be
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-03 18:38:39 +0300

    configured webpack to load es6 files and moved login.js into login.es6

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,19 @@ module.exports = {
 	entry: ['./utils.js', './app.js'],
 	output: {
 		filename: "bundle.js"
+	},
+
+	module:{
+		loaders: [
+			{
+				test: /\.es6$/,
+				exclude: 'node_modules',
+				loader: 'babel-loader'
+			}
+		]
+	},
+
+	resolve: {
+		extensions: ['', '.js', '.es6']
 	}
 }
