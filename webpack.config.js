b02b047564cecf17137451e3d9dc19a00108f986
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-03 20:08:47 +0300

    organize files for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 "use strict";
 
+let path = require('path');
+
 module.exports = {
+	context: path.resolve('js'),
 	entry: ['./utils.js', './app.js'],
 	output: {
+		path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
 		filename: "bundle.js"
+	},
+
+	devServer: {
+		contentBase: 'public'
 	},
 
 	module:{
