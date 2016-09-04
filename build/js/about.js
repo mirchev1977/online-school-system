c264847b398685907a7a974bd84a5634cabae172
Miroslav <mirchev1977@novihorizonti.eu>
2016-09-04 14:05:42 +0300

    deleted build/js

--- build/js/about.js
+++
@@ -1,8 +0,0 @@
-webpackJsonp([3],[/*!*******************!*\
-  !*** multi about ***!
-  \*******************/
-function(n,o,c){c(/*! ./utils.js */1),n.exports=c(/*! ./about_page.js */4)},,,,/*!***********************!*\
-  !*** ./about_page.js ***!
-  \***********************/
-function(n,o){}]);
-//# sourceMappingURL=about.js.map
--- build/js/contact.js
+++
@@ -1,8 +0,0 @@
-webpackJsonp([2],{0:/*!*********************!*\
-  !*** multi contact ***!
-  \*********************/
-function(n,o,c){c(/*! ./utils.js */1),n.exports=c(/*! ./contact_page.js */5)},5:/*!*************************!*\
-  !*** ./contact_page.js ***!
-  \*************************/
-function(n,o){}});
-//# sourceMappingURL=contact.js.map
